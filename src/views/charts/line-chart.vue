453069913f424f6160efc181a9ae9c881cdf780a
Ashley Marques <marques.ashley@gmail.com>
2023-09-18 14:58:27 -0300

    charts

---
+++ src/views/charts/line-chart.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="chart-container">
+    <line-chart height="100%" width="100%" />
+  </div>
+</template>
+
+<script lang="ts">
+import LineChart from '@/components/Charts/LineChart.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LineChartDemo',
+  components: {
+    LineChart
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.chart-container {
+  position: relative;
+  width: 100%;
+  height: calc(100vh - 84px);
+}
+</style>
---
+++ src/views/charts/mixed-chart.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="chart-container">
+    <mixed-chart height="100%" width="100%" />
+  </div>
+</template>
+
+<script lang="ts">
+import MixedChart from '@/components/Charts/MixedChart.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MixedChartDemo',
+  components: {
+    MixedChart
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.chart-container {
+  position: relative;
+  width: 100%;
+  height: calc(100vh - 84px);
+}
+</style>
