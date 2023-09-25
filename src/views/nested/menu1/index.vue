757e5955c48ccd0fdca75b909a54bc4d4b6c0f0c
Ashley Marques <marques.ashley@gmail.com>
2023-09-25 11:59:26 -0300

    nested menu

---
+++ src/views/nested/menu1/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="padding: 30px">
+    <el-alert :closable="false" title="menu 1">
+      <transition name="fade-transform" mode="out-in">
+        <router-view />
+      </transition>
+    </el-alert>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Menu1'
+})
+</script>
---
+++ src/views/nested/menu2/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div style="padding: 30px">
+    <el-alert :closable="false" title="menu 2" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Menu2'
+})
+</script>
